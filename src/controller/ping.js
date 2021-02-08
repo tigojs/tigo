5d894241fc70072407f4112a8c11a56d783780f5
BackRunner <dev@backrunner.top>
2021-02-08 22:47:27 +0800

    feat: add heartbeat

--- src/controller/ping.js
+++ src/controller/ping.js
@@ -4,16 +4,24 @@ const { successResponse } = require('@tigo/utils');
 class PingController extends BaseController {
   getRoutes() {
     return {
-      '/common/ping': {
+      '/common/checkAvailable': {
         type: 'get',
-        target: this.ping,
+        target: this.checkAvailable,
+      },
+      '/common/heartbeat': {
+        type: 'get',
+        target: this.heartbeat,
       }
     };
   }
-  async ping(ctx) {
+  async checkAvailable(ctx) {
     ctx.body = successResponse({
       auth: !!ctx.app.tigo.auth,
     });
+  }
+  async heartbeat(ctx) {
+    ctx.set('Content-Type', 'text/plain');
+    ctx.body = 1;
   }
 }
 
