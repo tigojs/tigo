3f2dc956a6d5c5b0535806ab844ecd390585820f
BackRunner <dev@backrunner.top>
2021-02-12 22:56:59 +0800

    feat: add lambda mysql package

---
+++ lambda-packages/mysql/main.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql2/promise');
+const LRU = require('lru-cache');
+
+const cache = new LRU({
+  max: 100,
+  maxAge: 1000 * 60 * 10,
+  dispose: function (key, conn) {
+    conn.end();
+  },
+  updateAgeOnGet: true,
+});
+
+const needValidate = ['host', 'user', 'database'];
+
+const validateConfig = (config) => {
+  needValidate.forEach((key) => {
+    if (!config.key) {
+      throw new Error(`Parameter [${key}] is necessary.`);
+    }
+  });
+}
+
+async function createConnection(config) {
+  if (!config || typeof config !== 'object') {
+    throw new Error('Config is empty.');
+  }
+  validateConfig(config);
+  const { host, port, user, database } = config;
+  const key = `${host}${port || 3306}${user}${database}`;
+  const cached = cache.get(key);
+  if (cached) {
+    return cached;
+  }
+  const conn = await mysql.createConnection(config);
+  conn.__end = conn.end;
+  conn.end = (...args) => {
+    conn.__end.call(conn, args);
+  }
+  cache.set(key, conn);
+  return conn;
+}
+
+module.exports = {
+  createConnection,
+};
