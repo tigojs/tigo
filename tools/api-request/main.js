bc0ce3c499fd7017f8c4a32e46a6303947437825
BackRunner <dev@backrunner.top>
2021-03-04 17:43:23 +0800

    minor: add jsdoc for opt

--- tools/api-request/main.js
+++ tools/api-request/main.js
@@ -14,9 +14,20 @@ const InsertPrefix = function (prefix) {
 const AutoSign = function (signer) {
   return function (request) {
     request.set(signer.getHeaders());
-  }
+  };
 };
 
+/**
+ * Request tigo API
+ * @param {object} opts Options
+ * @param {string} opts.prefix Prefix of API URL.
+ * @param {string} opts.host To build prefix automatically, you should fill in this option to specify the host.
+ * @param {number} opts.port Not necessary if port is 80 or 443.
+ * @param {boolean} opts.https If true, using https to request API.
+ * @param {string} opts.base Base path of API, like /api.
+ * @param {string} opts.ak Your tigo API AccessKey
+ * @param {string} opts.sk Your tigo API SecertKey
+ */
 const getAgent = (opts) => {
   // generate prefix
   let prefix;
