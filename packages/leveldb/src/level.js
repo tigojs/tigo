2f77f05b2faf9735576cbcfd6a69d1e07eb61d3a
BackRunner <dev@backrunner.top>
2021-03-11 19:16:35 +0800

    feat: fit latest changes

--- packages/leveldb/src/level.js
+++ packages/leveldb/src/level.js
@@ -1,71 +1,10 @@
 const levelup = require('levelup');
 const leveldown = require('leveldown');
-const moment = require('moment');
+const { extendLevelDb } = require('@tigojs/utils');
 
 function openDatabase(app, dbPath) {
   // open database
-  const db = levelup(leveldown(dbPath));
-
-  // extend
-  db.set = db.put;
-  db.hasObject = async (key) => {
-    let obj;
-    try {
-      obj = await db.getObject(key);
-    } catch (err) {
-      if (err.notFound) {
-        return false;
-      }
-      throw err;
-    }
-    if (!obj) {
-      return false;
-    }
-    return true;
-  }
-  db.putObject = async (key, value) => {
-    return await db.put(key, JSON.stringify(value));
-  }
-  db.setObject = db.putObject;
-  db.getObject = async (key) => {
-    try {
-      const obj = await db.get(key, {
-        asBuffer: false,
-      });
-      return JSON.parse(obj) || null;
-    } catch (err) {
-      if (err.notFound) {
-        return null;
-      }
-      throw err;
-    }
-  }
-  db.setExpires = (key, data, expires) => {
-    if (typeof expires !== 'number') {
-      throw new Error('Expires must be a number');
-    }
-    if (expires <= 0) {
-      return;
-    }
-    return db.setObject(key, {
-      expires,
-      value: data,
-      createAt: moment().valueOf(),
-    });
-  }
-  db.getExpires = async (key) => {
-    const stored = await db.getObject(key);
-    if (!stored) {
-      return null;
-    }
-    const { value, createAt, expires } = stored;
-    if (createAt + expires < moment().valueOf()) {
-      await db.del(key);
-      return null;
-    }
-    return value;
-  }
-
+  const db = extendLevelDb(levelup(leveldown(dbPath)));
   return db;
 }
 
