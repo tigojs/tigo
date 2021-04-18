27f755ba30f97b98e2545cedc161581ffedddc9b
BackRunner <dev@backrunner.top>
2021-04-18 17:04:26 +0800

    fix: postInstall logic

--- packages/mysql/scripts/postInstall.js
+++ packages/mysql/scripts/postInstall.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const doPrompt = async function () {
-  this.logger.info('Please input your settings for mysql:');
+  this.logger.info('Please input your configuration for mysql:');
   const answers = await this.inquirer.prompt([
     {
       type: 'input',
@@ -31,6 +31,12 @@ const doPrompt = async function () {
       type: 'input',
       name: 'database',
       message: 'Database',
+      validate: (database) => {
+        if (!database || (typeof database === 'string' && !database.trim())) {
+          return 'Database cannot be empty.';
+        }
+        return true;
+      },
     },
   ]);
   const doQueryPassword = async () => {
@@ -39,11 +45,23 @@ const doPrompt = async function () {
         type: 'password',
         name: 'password',
         message: 'Password',
+        validate: (password) => {
+          if (!password || (typeof password === 'string' && !password.trim())) {
+            return 'Password cannot be empty.';
+          }
+          return true;
+        },
       },
       {
         type: 'password',
         name: 'confirmPassword',
         message: 'Please reinput the password to confirm',
+        validate: (password) => {
+          if (!password || (typeof password === 'string' && !password.trim())) {
+            return 'Confirm password cannot be empty.';
+          }
+          return true;
+        },
       },
     ]);
     if (pwds.password !== pwds.confirmPassword) {
@@ -59,10 +77,10 @@ const doPrompt = async function () {
   };
 };
 
-const tryCreateDatabase = async function (conn, database) {
+const tryCreateDatabase = async function (connection, database) {
   const createDatabase = () => {
     return new Promise((resolve, reject) => {
-      conn.query(`CREATE DATABASE IF NOT EXISTS ${database} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`, (err, result) => {
+      connection.query(`CREATE DATABASE IF NOT EXISTS ${database} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`, (err, result) => {
         if (err) {
           reject(err);
         }
@@ -78,9 +96,10 @@ const tryCreateDatabase = async function (conn, database) {
   }
 };
 
-const tryConnect = async function ({ host, user, password, database }) {
+const tryConnect = async function ({ host, port, user, password, database }) {
   const connection = mysql.createConnection({
     host,
+    port,
     user,
     password,
   });
@@ -103,7 +122,7 @@ const tryConnect = async function ({ host, user, password, database }) {
   // check database
   const checkDatabase = () => {
     return new Promise((resolve, reject) => {
-      connection.query(`show databases like ${database};`, (err, result) => {
+      connection.query(`show databases like '${database}';`, (err, result) => {
         if (err) {
           reject(err);
         }
@@ -111,28 +130,31 @@ const tryConnect = async function ({ host, user, password, database }) {
       });
     });
   };
+  let res;
   try {
-    const res = await checkDatabase();
-    if (!res.length) {
-      const answers = await this.inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'confirm',
-          message: 'Unable to find the set database, do you want to create it automatically?',
-          default: true,
-        },
-      ]);
-      if (answers.confirm) {
-        await tryCreateDatabase.call(this, connection, database);
-      } else {
-        this.logger.warn('Please check your configuration or create the database manually.');
-      }
-    }
-    this.logger.info('Connection check finished.');
+    res = await checkDatabase();
   } catch (err) {
     this.logger.error('Failed to check database.', err);
+    connection.destroy();
     return;
   }
+  if (!res.length) {
+    const answers = await this.inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Unable to find the set database, do you want to create it automatically?',
+        default: true,
+      },
+    ]);
+    if (answers.confirm) {
+      await tryCreateDatabase.call(this, connection, database);
+    } else {
+      this.logger.warn('Please check your configuration or create the database manually.');
+    }
+  }
+  this.logger.info('Connection check finished.');
+  connection.destroy();
 };
 
 const postInstall = async function () {
@@ -149,7 +171,7 @@ const postInstall = async function () {
   });
   // try to connect database
   this.logger.debug('Try to connect database...');
-  await tryConnect.call(this);
+  await tryConnect.call(this, { host, port, user, password, database });
 };
 
 module.exports = postInstall;
