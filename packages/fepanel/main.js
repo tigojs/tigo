80bdd37e3b33359c7362cf9fb750bb42812aa348
BackRunner <dev@backrunner.top>
2021-04-20 14:58:18 +0800

    feat: support custom host

--- packages/fepanel/main.js
+++ packages/fepanel/main.js
@@ -3,64 +3,67 @@ const { collectStaticFiles } = require('@tigojs/utils');
 const fs = require('fs');
 
 const validateRules = {
-	distPath: {
-		required: true,
-		validator: (v) => {
-			return fs.existsSync(v) ? true : 'Dist path does not exist, please check it.';
-		},
-	},
-	domain: {
-		required: true,
-		validator: (v) => {
-			return isDomain(v);
-		},
-	},
+  distPath: {
+    required: true,
+    validator: (v) => {
+      return fs.existsSync(v) ? true : 'Dist path does not exist, please check it.';
+    },
+  },
+  domain: {
+    required: true,
+    validator: (v) => {
+      return isDomain(v);
+    },
+  },
 };
 
 const validate = (rules, values) => {
-	Object.keys(rules).forEach((key) => {
-		const rule = rules[key];
-		if (rule.required && !values[key]) {
-			throw new Error(`${key} is missing, please set it in the config.`);
-		}
-		if (rule.validator && typeof rule.validator === 'function') {
-			const res = rule.validator(values[key]);
-			if (res !== true) {
-				throw new Error(typeof res === 'string' ? res : `${key} is invalid, please check it.`);
-			}
-		}
-	});
-}
+  Object.keys(rules).forEach((key) => {
+    const rule = rules[key];
+    if (rule.required && !values[key]) {
+      throw new Error(`${key} is missing, please set it in the config.`);
+    }
+    if (rule.validator && typeof rule.validator === 'function') {
+      const res = rule.validator(values[key]);
+      if (res !== true) {
+        throw new Error(typeof res === 'string' ? res : `${key} is invalid, please check it.`);
+      }
+    }
+  });
+};
 
 const plugin = {
   dependencies: [
     {
       package: '@tigojs/hostbinder',
-			allowAutoImport: true,
+      allowAutoImport: true,
     },
   ],
-	mount(app, opts) {
-		validate(opts, validateRules);
-		const staticFiles = collectStaticFiles.call(app, opts.distPath);
-		app.static.fepanel = staticFiles;
-		// register proxy
-		const resolver = function(host, url, req) {
-			const formatted = `${host}${url}`;
-			if (/^tigo\.pwp\.app\/(css|fonts|js)\/.+$/.test(formatted)) {
-				return `http://127.0.0.1:${app.tigo.config.server.port}/static/fepanel/`;
-			} else if (/tigo\.pwp\.app\//.test(formatted)) {
-				return {
-					rewriteTo: `http://127.0.0.1:${app.tigo.config.server.port}/static/fepanel/index.html`,
-				};
-			}
-		}
-		resolver.priority = 100;
+  mount(app, opts) {
+    validate(opts, validateRules);
+    const staticFiles = collectStaticFiles.call(app, opts.distPath);
+    app.static.fepanel = staticFiles;
+    // register proxy
+    const resolver = function (host, url, req) {
+      const formattedPath = `${host}${url}`;
+      const formattedDomain = opts.domain.replaceAll('.', '\\.');
+      const staticFilesTester = new RegExp(`^${formattedDomain}\/(css|fonts|js)\/.+$`);
+      const domainTester = new RegExp(`^${formattedDomain}\/`);
+      if (staticFilesTester.test(formattedPath)) {
+        return `http://127.0.0.1:${app.tigo.config.server.port}/static/fepanel/`;
+      } else if (domainTester.test(formattedPath)) {
+        return {
+          rewriteTo: `http://127.0.0.1:${app.tigo.config.server.port}/static/fepanel/index.html`,
+        };
+      }
+    };
+    resolver.priority = 100;
     app.tigo.hostbinder.proxy.addResolver(resolver);
-		if (app.tigo.hostbinder.useHttps) {
-			const { email, production } = app.tigo.hostbinder.useHttps.letsencrypt;
-			app.tigo.hostbinder.proxy.updateCertificates('tigo.pwp.app', email, production);
-		}
-	},
+    if (app.tigo.hostbinder.useHttps) {
+      const { email, production } = app.tigo.hostbinder.useHttps.letsencrypt;
+      app.tigo.hostbinder.proxy.updateCertificates(opts.domain, email, production);
+    }
+  },
 };
 
 module.exports = plugin;
