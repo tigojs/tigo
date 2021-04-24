3f228f4695a25535eda95983bbe465b1fa06e162
BackRunner <dev@backrunner.top>
2021-04-25 02:37:10 +0800

    fix: set default env to prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,6 @@ module.exports = {
       name: 'tigo-server',
       script: './server.js',
       env: {
-        NODE_ENV: 'dev',
-        DB_ENV: 'prod',
-      },
-      env_production: {
         NODE_ENV: 'prod',
         DB_ENV: 'prod',
       },
