d6111e2045ac072e5684fdd50159ad23c0c187f8
BackRunner <dev@backrunner.top>
2021-05-16 00:52:12 +0800

    feat: kv storage now using mongodb to store data

--- packages/faas/src/classes/KV.js
+++ packages/faas/src/classes/KV.js
@@ -2,27 +2,22 @@ const LRUCache = require('lru-cache');
 const wrapper = require('../utils/classWrapper');
 const ctx = Symbol('koaContext');
 const lambdaId = Symbol('lambdaId');
+const collection = Symbol('collection');
 
-const getKvKey = (lambdaId, key) => `faas_kv_${lambdaId}_${key}`;
-
-const checkKey = (key) => {
+const validateKey = (key) => {
   if (typeof key !== 'string') {
     throw new Error('Lambda KV Storage only accepts string key.');
-  }
-};
-const checkValue = (value) => {
-  if (typeof value !== 'string') {
-    throw new Error('Lambda KV Storage only accepts string value.');
   }
 };
 
 class KV {
   constructor(context, _lambdaId, config) {
-    if (!context.tigo.faas.kvStorage) {
+    if (!context.tigo.faas.lambdaKvEngine) {
       throw new Error('Lambda KV Storage is not enabled on this server.');
     }
     this[ctx] = context;
     this[lambdaId] = _lambdaId;
+    this[collection] = this[ctx].tigo.faas.lambdaKvEngine.collection(this[lambdaId]);
     // init cache
     const cacheConfig = config?.cache || {};
     this.cache = new LRUCache({
@@ -32,35 +27,57 @@ class KV {
     });
   }
   async get(key) {
-    checkKey(key);
+    validateKey(key);
     const cacheKey = `${this[lambdaId]}_${key}`;
     const cached = this.cache.get(cacheKey);
     if (cached) {
       return cached;
     }
     // no cached value
-    const value = await this[ctx].tigo.faas.kvStorage.getString(getKvKey(this[lambdaId], key));
-    if (value) {
+    let { value } = await this[collection].findOne({
+      key,
+    });
+    if (typeof value !== 'undefined' && value !== null) {
       this.cache.set(cacheKey, value);
     }
     return value;
   }
   async set(key, value) {
-    checkKey(key);
-    checkValue(value);
+    validateKey(key);
     try {
-      await this[ctx].tigo.faas.kvStorage.put(getKvKey(this[lambdaId], key), value);
+      await this[collection].updateOne(
+        {
+          key,
+        },
+        {
+          $set: {
+            value,
+            updatedTime: Date.now(),
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
     } catch (err) {
       this[ctx].logger.error('Failed to set value.', err);
-      throw new Error('Failed to set value.');
+      if (process.env.NODE_ENV === 'dev') {
+        throw err;
+      } else {
+        throw new Error('Failed to set value.');
+      }
     }
     const cacheKey = `${this[lambdaId]}_${key}`;
     this.cache.del(cacheKey);
   }
   async remove(key) {
-    checkKey(key);
+    validateKey(key);
     try {
-      await this[ctx].tigo.faas.kvStorage.del(getKvKey(this[lambdaId], key));
+      await this[collection].deleteOne(
+        {
+          key,
+        }
+      );
     } catch (err) {
       this[ctx].logger.error('Failed to remove key.', err);
       throw new Error('Failed to remove key from kv storage.');
