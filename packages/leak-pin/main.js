be74af28ba26895fd6a005769eee29d632b46401
BackRunner <dev@backrunner.top>
2022-03-21 22:50:03 +0800

    feat: add leak pin plugin

---
+++ packages/leak-pin/main.js
@@ -0,0 +1,24 @@
+const memwatch = require('node-memwatch-new');
+const heapdump = require('heapdump');
+const path = require('path');
+const fs = require('fs');
+
+const plugin = {
+  type: 'module',
+  mount(app) {
+    const snapshotDir =  path.resolve(app.config.runDirPath, './snapshots');
+    if (!fs.existsSync(snapshotDir)) {
+      fs.mkdirSync(snapshotDir, { recursive: true });
+    }
+    memwatch.on('leak', () => {
+      heapdump.writeSnapshot(path.resolve(snapshotDir, `./${Date.now()}.heapsnapshot`), (err, fileName) => {
+        if (err) {
+          app.logger.error('Failed to save heap snapshot.', err);
+        }
+        app.logger.info(`Memory leak detected, snapshot has saved to ${fileName}`);
+      });
+    });
+  },
+};
+
+module.exports = plugin;
